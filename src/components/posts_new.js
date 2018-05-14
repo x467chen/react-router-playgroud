00b9379959672ed305f25c5bd092d61db3de5f2e
Kate <chenxuanqi1993cookie@gmail.com>
2018-05-14 16:38:44 -0400

    add post new page and switch funciton for routers

---
+++ src/components/posts_new.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+
+    return (
+      <div>
+        post PostsNew!
+
+      </div>
+    );
+  }
+}
+
+
+
+export default PostsNew;
