92714ac67e12ac0cb2b360d3d699ff3dff8373fd
Kate <chenxuanqi1993cookie@gmail.com>
2018-05-14 13:50:03 -0400

    add main page route

--- src/index.js
+++ src/index.js
@@ -2,34 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 import {BrowserRouter, Route} from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
+import PostsIndex from './components/post_index';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-class Hello extends React.Component{
-  render(){
-    return <div>Hello!</div>
-  }
-}
-
-class Goodbye extends React.Component{
-  render(){
-    return <div>Goodbye!</div>
-  }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        Header
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye" component={Goodbye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
