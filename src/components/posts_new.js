3d13261b6ee6f8cbc768d9c0a5d564dd1df17f5f
Kate <chenxuanqi1993cookie@gmail.com>
2018-05-15 13:08:50 -0400

    link the component to the reducers for submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -16,10 +16,14 @@ class PostsNew extends Component {
     );
   }
 
+  onSubmit(values){
+    console.log(values);
+  }
 
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label="Title"
           name="title"
@@ -35,6 +39,7 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
   }
