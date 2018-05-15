2b7ec7b9bdc1bbf9fd673a9d3745466d40400883
Kate <chenxuanqi1993cookie@gmail.com>
2018-05-15 11:53:12 -0400

    add redux form into reducers and component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field){
+    return(
+      <div className="form-group">
+          <label>{field.label}</label>
+          <input
+            className="form-control"
+            type="text"
+            {...field.input}
+          />
+        </div>
+    );
+  }
+
+
   render() {
-
     return (
-      <div>
-        post PostsNew!
-
-      </div>
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+      </form>
     );
   }
 }
 
 
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
